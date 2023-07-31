29431e87ce15e2368eed774d5becaba7012d79aa
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-07-31 20:37:55 -0300

    123

---
+++ src/componets/Nav.jsx
@@ -0,0 +1,25 @@
+import "../App.css";
+const Nav = () => {
+  return (
+    <>
+      <nav className="navbar navbar-light bg-light fixed-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img
+              src="src/logo.jpg"
+              alt=""
+              width="50"
+              height="50"
+              className="d-inline-block rounded-circle"
+            />
+            <span className="navbar-brand mb-0 h1 logo ps-2">
+              CEFERINO HOTEL
+            </span>
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
