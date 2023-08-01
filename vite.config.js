6723b676ac02c5929a69b8107173d562f08347b4
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-07-31 22:08:41 -0300

    1234

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Nataniel44.github.io/",
+  base: "https://Nataniel44.github.io/hotel-ceferino/",
 });
